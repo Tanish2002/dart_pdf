33eb887c7d3177fd1a8081a5c0806aa6c3737ac4
Tanish2002 <tanishkhare@gmail.com>
2024-02-24 23:31:16 +0530

    make pdfium download optional

--- printing/windows/DownloadProject.cmake
+++ printing/windows/DownloadProject.cmake
@@ -94,65 +94,74 @@ set(_DownloadProjectDir "${CMAKE_CURRENT_LIST_DIR}")
 include(CMakeParseArguments)
 
 function(download_project)
-
+  
     set(options QUIET)
     set(oneValueArgs
-        PROJ
-        PREFIX
-        DOWNLOAD_DIR
-        SOURCE_DIR
-        BINARY_DIR
-        # Prevent the following from being passed through
-        CONFIGURE_COMMAND
-        BUILD_COMMAND
-        INSTALL_COMMAND
-        TEST_COMMAND
+      PROJ
+      PREFIX
+      DOWNLOAD_DIR
+      SOURCE_DIR
+      BINARY_DIR
+      # Prevent the following from being passed through
+      CONFIGURE_COMMAND
+      BUILD_COMMAND
+      INSTALL_COMMAND
+      TEST_COMMAND
     )
     set(multiValueArgs "")
-
+  
     cmake_parse_arguments(DL_ARGS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
+  
     # Hide output if requested
     if (DL_ARGS_QUIET)
-        set(OUTPUT_QUIET "OUTPUT_QUIET")
+      set(OUTPUT_QUIET "OUTPUT_QUIET")
     else()
-        unset(OUTPUT_QUIET)
-        message(STATUS "Downloading/updating ${DL_ARGS_PROJ}")
+      unset(OUTPUT_QUIET)
+      message(STATUS "Downloading/updating ${DL_ARGS_PROJ}")
     endif()
-
+  
+    # Check for environment variable (e.g., PDFIUM_LOCAL_PATH)
+    if(DEFINED $ENV{PDFIUM_LOCAL_PATH})
+      if(EXISTS $ENV{PDFIUM_LOCAL_PATH})
+        message(STATUS "Using PDFIUM from environment variable: ${ENV{PDFIUM_LOCAL_PATH}}")
+        set(DL_ARGS_SOURCE_DIR ${ENV{PDFIUM_LOCAL_PATH}} PARENT_SCOPE)  
+      else()
+        message(FATAL_ERROR "Environment variable PDFIUM_LOCAL_PATH is set, but the file does not exist.")
+      endif()
+    else()
     # Set up where we will put our temporary CMakeLists.txt file and also
     # the base point below which the default source and binary dirs will be.
     # The prefix must always be an absolute path.
     if (NOT DL_ARGS_PREFIX)
-        set(DL_ARGS_PREFIX "${CMAKE_BINARY_DIR}")
+      set(DL_ARGS_PREFIX "${CMAKE_BINARY_DIR}")
     else()
-        get_filename_component(DL_ARGS_PREFIX "${DL_ARGS_PREFIX}" ABSOLUTE
-                               BASE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
+      get_filename_component(DL_ARGS_PREFIX "${DL_ARGS_PREFIX}" ABSOLUTE
+                             BASE_DIR "${CMAKE_CURRENT_BINARY_DIR}")
     endif()
     if (NOT DL_ARGS_DOWNLOAD_DIR)
-        set(DL_ARGS_DOWNLOAD_DIR "${DL_ARGS_PREFIX}/${DL_ARGS_PROJ}-download")
+      set(DL_ARGS_DOWNLOAD_DIR "${DL_ARGS_PREFIX}/${DL_ARGS_PROJ}-download")
     endif()
-
+  
     # Ensure the caller can know where to find the source and build directories
     if (NOT DL_ARGS_SOURCE_DIR)
-        set(DL_ARGS_SOURCE_DIR "${DL_ARGS_PREFIX}/${DL_ARGS_PROJ}-src")
+      set(DL_ARGS_SOURCE_DIR "${DL_ARGS_PREFIX}/${DL_ARGS_PROJ}-src")
     endif()
     if (NOT DL_ARGS_BINARY_DIR)
-        set(DL_ARGS_BINARY_DIR "${DL_ARGS_PREFIX}/${DL_ARGS_PROJ}-build")
+      set(DL_ARGS_BINARY_DIR "${DL_ARGS_PREFIX}/${DL_ARGS_PROJ}-build")
     endif()
     set(${DL_ARGS_PROJ}_SOURCE_DIR "${DL_ARGS_SOURCE_DIR}" PARENT_SCOPE)
     set(${DL_ARGS_PROJ}_BINARY_DIR "${DL_ARGS_BINARY_DIR}" PARENT_SCOPE)
-
+  
     # The way that CLion manages multiple configurations, it causes a copy of
     # the CMakeCache.txt to be copied across due to it not expecting there to
-    # be a project within a project.  This causes the hard-coded paths in the
-    # cache to be copied and builds to fail.  To mitigate this, we simply
-    # remove the cache if it exists before we configure the new project.  It
-    # is safe to do so because it will be re-generated.  Since this is only
+    # be a project within a project. This causes the hard-coded paths in the
+    # cache to be copied and builds to fail. To mitigate this, we simply
+    # remove the cache if it exists before we configure the new project. It
+    # is safe to do so because it will be re-generated. Since this is only
     # executed at the configure step, it should not cause additional builds or
     # downloads.
     file(REMOVE "${DL_ARGS_DOWNLOAD_DIR}/CMakeCache.txt")
-
+  
     # Create and build a separate CMake project to carry out the download.
     # If we've already previously done these steps, they will not cause
     # anything to be updated, so extra rebuilds of the project won't occur.
@@ -161,14 +170,14 @@ function(download_project)
     configure_file("${_DownloadProjectDir}/DownloadProject.CMakeLists.cmake.in"
                    "${DL_ARGS_DOWNLOAD_DIR}/CMakeLists.txt")
     execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}"
-                        -D "CMAKE_MAKE_PROGRAM:FILE=${CMAKE_MAKE_PROGRAM}"
-                        .
+                    -D "CMAKE_MAKE_PROGRAM:FILE=${CMAKE_MAKE_PROGRAM}"
+                    .
                     RESULT_VARIABLE result
                     ${OUTPUT_QUIET}
                     WORKING_DIRECTORY "${DL_ARGS_DOWNLOAD_DIR}"
     )
     if(result)
-        message(FATAL_ERROR "CMake step for ${DL_ARGS_PROJ} failed: ${result}")
+      message(FATAL_ERROR "CMake step for ${DL_ARGS_PROJ} failed: ${result}")
     endif()
     execute_process(COMMAND ${CMAKE_COMMAND} --build .
                     RESULT_VARIABLE result
@@ -176,7 +185,7 @@ function(download_project)
                     WORKING_DIRECTORY "${DL_ARGS_DOWNLOAD_DIR}"
     )
     if(result)
-        message(FATAL_ERROR "Build step for ${DL_ARGS_PROJ} failed: ${result}")
+      message(FATAL_ERROR "Build step for ${DL_ARGS_PROJ} failed: ${result}")
     endif()
-
-endfunction()
+    endif() # End of else() for the environment variable check
+endfunction() 
